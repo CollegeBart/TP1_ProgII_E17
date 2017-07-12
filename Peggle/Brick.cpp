63c685ece38c58eca25844d4a9ec6b65ed5563c4
Anthony Robert <tonylord.ar@gmail.com>
2017-07-11 23:36:05 -0400

    added something for the synthese and the brick collision try

--- Peggle/Brick.cpp
+++ Peggle/Brick.cpp
@@ -10,6 +10,7 @@ Brick::Brick()
 		D3DFMT_UNKNOWN, D3DPOOL_MANAGED, D3DX_FILTER_NONE, D3DX_DEFAULT,
 		D3DCOLOR_XRGB(255, 255, 255), &info, NULL, &texture));
 
+	//m_topleft = D3DXVECTOR2(info.Width, info.Height);
 	center = D3DXVECTOR3(info.Width / 2, info.Height / 2, 0.f);
 }
 
@@ -18,15 +19,27 @@ Brick::~Brick()
 	ReleaseCOM(texture);
 }
 
-bool Brick::CollidesWith(const Brick & bricks) const
+bool Brick::CollidesWith(const Brick*  bricks) const
 {
 	bool hit = false;
 	float lenght, height;
-
-
-
-
-
+	//
+	//lenght = m_topleft.x > bricks->GetX()
+	//	?
+	//		(m_topLeft.x + (m_size.x / 2)) - (bricks->GetX() + (bricks->GetWidth() / 2))
+	//	:
+	//		(bricks->GetX() + (bricks->GetWidth() / 2)) - (m_topLeft.x + (m_size.x / 2));
+	//
+	//height = m_topLeft.y > bricks->GetY()
+	//	?
+	//	(m_topLeft.y + (m_size.y / 2)) - (bricks->GetY() + (bricks->GetHeight() / 2))
+	//	:
+	//	(bricks->GetY() + (bricks->GetHeight() / 2)) - (m_topLeft.y + (m_size.y / 2));
+	//
+	//return	((2 * height) <= (bricks->GetHeight() + m_size.y))
+	//	&&
+	//	((2 * lenght) <= (bricks->GetWidth() + m_size.x));
+	//
 	return false;
 }
 
--- Peggle/Brick.h
+++ Peggle/Brick.h
@@ -5,9 +5,14 @@ public:
 	Brick();
 	~Brick();
 
-	bool CollidesWith(const Brick& bricks) const;
+	bool CollidesWith(const Brick* bricks) const;
 	void Draw(ID3DXSprite* spriteBatch);
 	void SetPosition(D3DXVECTOR3 pos);
+	
+	float GetX() const { return m_topleft.x; }
+	float GetY() const { return m_topleft.y; }
+	float GetWidth() const { return m_size.x; }
+	float GetHeight() const { return m_size.y; }
 private:
 	// Sprite
 	IDirect3DTexture9* texture;
@@ -18,6 +23,7 @@ private:
 	D3DXVECTOR3 position3;
 	D3DXVECTOR3 position4;
 	D3DXVECTOR3 center;
+
 
 	D3DXVECTOR2 m_topleft, m_size;
 
